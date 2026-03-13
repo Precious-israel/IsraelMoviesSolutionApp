cf42e961dc19a431ded45fe7010e3e0fcfb8b733
precious-israel <carisabiodun@gmail.com>
2026-03-13 11:06:40 +0100

    Initial commit of my Movie App with enhancements

---
+++ Models/SeedData.cs
@@ -0,0 +1,83 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Romantic Comedy",
+                    Price = 7.99M,
+                    Rating = "R"
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters ",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Price = 8.99M,
+                    Rating = "PG"
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters 2",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Price = 9.99M,
+                    Rating = "PG"
+                },
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Price = 3.99M,
+                    Rating = "PG-13"
+                },
+                new Movie
+                {
+                    Title = "Abatoir",
+                    ReleaseDate = DateTime.Parse("2020-1-1"),
+                    Genre = "Christian Film",
+                    Price = 9.99M,
+                    Rating = "PG-13"
+                },
+                new Movie
+                {
+                    Title = "The Men that built America",
+                    ReleaseDate = DateTime.Parse("1999-1-1"),
+                    Genre = "Documentary",
+                    Price = 8.99M,
+                    Rating = "R"
+                },
+                new Movie
+                {
+                    Title = "Interstellar",
+                    ReleaseDate = DateTime.Parse("2014-11-7"),
+                    Genre = "Sci-Fi",
+                    Price = 10.99M,
+                    Rating = "PG-13"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
